e0fd1130c919b1d473954d8bebb0db6999489573
ayaznoori <ayaz.noori15@gmail.com>
2022-10-20 10:11:54 +0530

    cors problem

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
-
+const cors=require('cors');
+server.use(cors())
 server.use(middlewares);
 server.use(router);
 
